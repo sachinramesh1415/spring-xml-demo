5fd011649c1d0525327dc41b7055835439da8cdd
Sachin R <sachin.r@cgi.com>
2019-09-30 15:49:33 +0530

    Fixed Identifier issue for classes.

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,9 +1,23 @@
 package com.stackroute.domain;
 
+import java.util.UUID;
+
 public class Actor {
+    private int actorId;
     private String name;
     private String gender;
     private int age;
+    public Actor() {
+        UUID idOne = UUID.randomUUID();
+        String str=""+idOne;
+        int uid=str.hashCode();
+        String filterStr=""+uid;
+        str=filterStr.replaceAll("-", "");
+        this.actorId = Integer.parseInt(str);
+    }
+    public int getActorId() {
+        return actorId;
+    }
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,9 +1,23 @@
 package com.stackroute.domain;
 
+import java.util.UUID;
+
 public class Movie {
+    private int movieId;
     private Actor actor;
+    public Movie() {
+        UUID idOne = UUID.randomUUID();
+        String str=""+idOne;
+        int uid=str.hashCode();
+        String filterStr=""+uid;
+        str=filterStr.replaceAll("-", "");
+        this.movieId = Integer.parseInt(str);
+    }
     public void setActor(Actor actor) {
         this.actor = actor;
+    }
+    public int getMovieId() {
+        return movieId;
     }
     public String getActorDetails() {
         System.out.println("Actor: " + actor.getName());
